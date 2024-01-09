8f09f58c1de6ba0a702226be6cdbb268b88ba24f
Mark Robinson <mark.robinson@codeup.com>
2024-01-09 16:03:51 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import {useState} from 'react';
+import AnimalShow from './AnimalShow';
+import './App.css';
+
+const allAnimals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+function getRandomAnimal() {
+  return allAnimals[Math.trunc(Math.random() * allAnimals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />
+  });
 
   const handleClick = () => {
     // console.log("button clicked");
-    setCount(count + 1);
+    // setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>
-        <p>Count is {count}</p>
+      <div className='animal-list'>
+        {renderAnimals}
       </div>
     </div>
   );
